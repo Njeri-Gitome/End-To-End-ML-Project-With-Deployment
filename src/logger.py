3a989efeff756f4d0605ffd49aca4e8440da043d
Grace Gitome <gitomegracie@gmail.com>
2023-11-01 01:07:25 +0300

    logger script

--- src/logger.py
+++ src/logger.py
@@ -7,7 +7,7 @@ from datetime import datetime
 #Definition of how the log file will be created
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path =os.path.join(os.getcwd(),"logs",LOG_FILE) #cwd -current working directory
-os.makedires(logs_path, exist_ok =True) #Even if there is an existing folder, keep on updating and creating the log files
+os.makedirs(logs_path, exist_ok =True) #Even if there is an existing folder, keep on updating and creating the log files
 
 LOG_FILE_PATH =os.path.join(logs_path,LOG_FILE)
 
