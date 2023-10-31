08943bd3e209557fe190fc786f162aad8c3697b7
Grace Gitome <gitomegracie@gmail.com>
2023-11-01 00:53:07 +0300

    component files and the python exceptions script

--- setup.py
+++ setup.py
@@ -1,9 +1,33 @@
 #This file will be responsible in creating the ML application as a package with the help of Python PyPi.
 
 from setuptools import find_packages, setup 
+from typing import List
 
 #find_package will find all the packages present in the ML project. 
 
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    get_requirements= []
+    with open(file_path) as file_obj: #open the file as a temporary object
+        requirements = file_obj.readlines()
+        #the for loop in order to prevent a new line (\n) being added after readlines function is implement as each line is being read
+        [req.replace("\n", "")for req in requirements]
+        
+        #For loop to mitigate printing of the '-e .' present in the requirements.txt file
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+            
+           
+            return requirements
+            
+            
+        
+
+        
 #Do the setup
 setup(
     name = 'mlproject',
@@ -11,6 +35,6 @@ setup(
     author = 'Grace',
     author_email= 'gitomegracie@gmail.com',
     packages = find_packages(),
-    install_requires = ['pandas', 'numpy', 'seaborn']
-    
+    install_requires = get_requirements('requirements.txt')
+        
     )
---
+++ src/exception.py
@@ -0,0 +1,28 @@
+import sys
+
+#Exceptions - a type of error that occurs when a syntactically correct Python code raises an error
+#sys module in Python provides various functions and variables that are used to manipulate different parts of the Python runtime environment.
+
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info()   #indicates which file and line the error is from
+    
+    #Creating variables that will be used in the format() function
+    file_name =exc_tb.tb_frame.f_code.co_filename                 #to be able to fetch the file name, present under custom exception handling documentation in Python
+        
+    error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)   
+        
+    )
+    return error_message
+
+
+#Creating a user defined class
+    
+class CustomException(Exception): # The class inherits the parent exception that was defined in the function above
+    def __init__(self, error_message, error_detail:sys ):
+        super.__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail= error_detail)
+        
+    def __str__(self):
+        return self.error_message
+    
